93ce93e1b07e73788b5951bb42ad8658a705cecf
Alec Edwards <ealec95@gmail.com>
2015-02-19 20:01:45 -0500

    Adds the Main Menu Function
    Adds the functioning main menu.

--- gme.py
+++ gme.py
@@ -82,5 +82,114 @@ def main():
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
+# this is a prototype code for the start menu in the pong game:
+#It will have 4 options; Single Player (AI controlled), Multiplayer (Same keyboard), High Scores (From MongoDB), Options (Player colors, Difficulty(?) )
+
+def Main_Menu():
+    #Menu triggers, when true, the program should call the function that corresponds to the option (single = single player, multi = multiplayer etc.)
+    single = False
+    multi = False
+    Scores = False
+    Settings = False
+
+    #Sets the screen size and updates accordingly
+    #DisplaySurface(or whatever variable name is decided upon) should be a global variable so that we are all using the same display and not creating different ones
+    DisplaySurface = pygame.display.set_mode((1280, 720))
+    pygame.display.update()
+
+    #Gives the main menu colors (White for text, Red for arrows) named attributes
+    White = (255, 255, 255)
+    Red = (255, 0, 0)
+
+    #Declares DisplaySurface as global so the screen size does not change from function to function
+    global DisplaySurface
+
+    #FPS clock, may be different from method/function FPS clocks
+    FPStimer = pygame.time.Clock()
+
+    DisplaySurface.fill((0, 0, 0))
+
+    Arrow_Position1 = 220
+    Arrow_Position2 = 210
+    Arrow_Position3 = 230
+    Arrow_Position4 = 200 
+
+    #Menu loop
+    while single == False and multi == False and Scores == False and Settings == False:
+        #Draws the title above the menu
+            #Draws the 'P' in pong master
+            DisplaySurface.fill((0, 0, 0))
+            pygame.draw.rect(DisplaySurface, White, ( (213, 150), (854, 20) ) )
+
+            #Sets the font for the title to 86 pixels high and standard font
+            font = pygame.font.Font(None, 86)
+    
+            #prints the title
+            text = font.render("Ping Pong Master", False, White, (0, 0, 0))
+    
+            #sets the subtext for the title to 32 pixels high
+            font = pygame.font.Font(None, 32)
+            font.set_italic(True)
+            Extreme = font.render("Game of the Year Edition", False, White, (0, 0, 0) )
+    
+        #Sets the font size for the rest of the menu items to 72px
+            font = pygame.font.Font(None, 72)
+            Single_Player = font.render("Single Player", False, White, (0, 0, 0) )
+    
+            Multiplayer = font.render("Multiplayer", False, White, (0, 0, 0) )
+    
+            High_Scores = font.render("High Scores", False, White, (0, 0, 0) )
+    
+            Options = font.render("Options", False, White, (0, 0, 0) )
+    
+            DisplaySurface.blit(Options, (500, 500))
+            DisplaySurface.blit(High_Scores, (500, 400) )
+            DisplaySurface.blit(Multiplayer, (500, 300))
+            DisplaySurface.blit(Single_Player, (500, 200))
+            DisplaySurface.blit(text, (213, 100))
+            DisplaySurface.blit(Extreme, (727, 120))
+    
+            #Draws the red selection arrow and determines the location with Arrow_Position
+            pygame.draw.rect(DisplaySurface, Red, ( ( 400, Arrow_Position1), (70, 10) ) )
+            pygame.draw.rect(DisplaySurface, Red, ( ( 400, Arrow_Position2), (60, 10) ) )
+            pygame.draw.rect(DisplaySurface, Red, ( ( 400, Arrow_Position3), (60, 10) ) )
+            pygame.draw.rect(DisplaySurface, Red, ( ( 440, Arrow_Position4), (10, 50) ) )
+            pygame.display.update()
+    
+    
+            for event in pygame.event.get():
+                #Senses when a key is pressed
+                if event.type == pygame.KEYDOWN:
+                    #Allows downward movement of the arrow and restricts movement past the last item in the screen (Options)
+                    if event.key == pygame.K_DOWN and Arrow_Position1 != 520:
+                        Arrow_Position1 += 100
+                        Arrow_Position2 += 100
+                        Arrow_Position3 += 100
+                        Arrow_Position4 += 100
+            
+                        #Allow upward movement of the arrow and restricts movement past the first item in the screen (Single Player)
+                    elif event.key == pygame.K_UP and Arrow_Position1 != 220:
+                        Arrow_Position1 -= 100
+                        Arrow_Position2 -= 100
+                        Arrow_Position3 -= 100
+                        Arrow_Position4 -= 100
+                
+                        #Determines what option the user picked when they hit enter(return) on their keyboard 
+                    elif event.key == pygame.K_RETURN and Arrow_Position1 == 220:
+                        single = True
+                    elif event.key == pygame.K_RETURN and Arrow_Position1 == 320:
+                        multi = True
+                    elif event.key == pygame.K_RETURN and Arrow_Position1 == 420:
+                        Scores = True
+                    elif event.key == pygame.K_RETURN and Arrow_Position1 == 520:
+                        Settings = True
+            
+                elif event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+    #When the single player in built, this function will call it at this point. When the single player match ends, a menu should appear saying something like:
+    #"Rematch or Main Menu" at which point it will call this function all over again.
+
 if __name__=='__main__':
     main()
